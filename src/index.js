1c1acebc41af83c1d018b2bdd4e0d456c54def4d
Stephen <steve@rebelops.io>
2022-01-03 16:55:53 -0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Twitter = require('twitter');
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 const client = new Twitter({
   consumer_key: core.getInput('consumer-key'),
@@ -8,12 +9,15 @@ const client = new Twitter({
   access_token_secret: core.getInput('access-token-secret'),
 });
 
+const repoName = github.context.payload.repository.full_name;
+const { message } = github.context.payload.commits[0];
+
 async function run() {
   try {
     client.post(
       'statuses/update',
       {
-        status: 'This tweet was posted when a pull request was approved!',
+        status: `A new PR was merged into ${repoName}: ${message}`,
       },
       (error, tweet, response) => {
         if (error) {
